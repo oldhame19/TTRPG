71a049f2732441e6b5e82c008062d6482211f98c
oldhame19 <oldhameli10@gmail.com>
2025-08-04 18:56:45 -0500

    Combat Forecast 2
    Reads stats properly now and changed layout

--- GUI/CombatUI/combat_forecast_panel.gd
+++ GUI/CombatUI/combat_forecast_panel.gd
@@ -15,20 +15,19 @@ class_name CombatForecastPanel
 @onready var enemy_hit_label = $CombatForecastPanel/MarginContainer/GridContainer/EnemyHIT
 @onready var enemy_crit_label = $CombatForecastPanel/MarginContainer/GridContainer/EnemyCRIT
 
-
 func update_forecast(forecast: CombatCalculator.CombatForecast) -> void:
 	# Ally side
-	ally_name_label.text = forecast.attacker_name
-	ally_weapon_label.text = forecast.attacker_weapon
-	ally_hp_label.text = "%s/%s" % [str(forecast.attacker_hp_current), str(forecast.attacker_hp_max)]
-	ally_atk_label.text = str(forecast.damage)
+	ally_name_label.text = forecast.ally_name
+	ally_weapon_label.text = forecast.ally_weapon
+	ally_hp_label.text = "%s/%s" % [str(forecast.ally_hp_current), str(forecast.ally_hp_max)]
+	ally_atk_label.text = str(forecast.attack)  # changed from damage
 	ally_hit_label.text = str(forecast.hit)
 	ally_crit_label.text = str(forecast.crit)
 
 	# Enemy side
-	enemy_name_label.text = forecast.defender_name
-	enemy_weapon_label.text = forecast.defender_weapon
-	enemy_hp_label.text = "%s/%s" % [str(forecast.defender_hp_current), str(forecast.defender_hp_max)]
-	enemy_atk_label.text = str(forecast.enemy_damage)
+	enemy_name_label.text = forecast.enemy_name
+	enemy_weapon_label.text = forecast.enemy_weapon
+	enemy_hp_label.text = "%s/%s" % [str(forecast.enemy_hp_current), str(forecast.enemy_hp_max)]
+	enemy_atk_label.text = str(forecast.enemy_attack)  # changed from enemy_damage
 	enemy_hit_label.text = str(forecast.enemy_hit)
 	enemy_crit_label.text = str(forecast.enemy_crit)
--- Globals/combat_calculator.gd
+++ Globals/combat_calculator.gd
@@ -1,140 +1,143 @@
+#combat_calculator.gd
 extends Node
 
 class CombatForecast:
-	var damage: Variant
+	var attack: Variant
 	var hit: Variant
 	var crit: Variant
-	var attacker_name: Variant
-	var defender_name: Variant
-	var attacker_weapon: Variant
-	var defender_weapon: Variant
-	var attacker_hp_current: Variant
-	var attacker_hp_max: Variant
-	var defender_hp_current: Variant
-	var defender_hp_max: Variant
-	var enemy_damage: Variant
+	var ally_name: Variant
+	var enemy_name: Variant
+	var ally_weapon: Variant
+	var enemy_weapon: Variant
+	var ally_hp_current: Variant
+	var ally_hp_max: Variant
+	var enemy_hp_current: Variant
+	var enemy_hp_max: Variant
+	var enemy_attack: Variant
 	var enemy_hit: Variant
 	var enemy_crit: Variant
 
 	func _init(
-		damage, hit, crit,
-		attacker_name, defender_name,
-		attacker_weapon, defender_weapon,
-		attacker_hp_current, attacker_hp_max,
-		defender_hp_current, defender_hp_max,
-		enemy_damage, enemy_hit, enemy_crit
+		attack, hit, crit,
+		ally_name, enemy_name,
+		ally_weapon, enemy_weapon,
+		ally_hp_current, ally_hp_max,
+		enemy_hp_current, enemy_hp_max,
+		enemy_attack, enemy_hit, enemy_crit
 	):
-		self.damage = damage
+		self.attack = attack
 		self.hit = hit
 		self.crit = crit
-		self.attacker_name = attacker_name
-		self.defender_name = defender_name
-		self.attacker_weapon = attacker_weapon
-		self.defender_weapon = defender_weapon
-		self.attacker_hp_current = attacker_hp_current
-		self.attacker_hp_max = attacker_hp_max
-		self.defender_hp_current = defender_hp_current
-		self.defender_hp_max = defender_hp_max
-		self.enemy_damage = enemy_damage
+		self.ally_name = ally_name
+		self.enemy_name = enemy_name
+		self.ally_weapon = ally_weapon
+		self.enemy_weapon = enemy_weapon
+		self.ally_hp_current = ally_hp_current
+		self.ally_hp_max = ally_hp_max
+		self.enemy_hp_current = enemy_hp_current
+		self.enemy_hp_max = enemy_hp_max
+		self.enemy_attack = enemy_attack
 		self.enemy_hit = enemy_hit
 		self.enemy_crit = enemy_crit
-		
 
-static func calculate_stats(attacker: Unit, defender: Unit) -> Dictionary:
-	if attacker == null or defender == null:
-		return {"damage": "--", "hit": "--", "crit": "--"}
+static func calculate_stats(ally: Unit, enemy: Unit) -> Dictionary:
+	if ally == null or ally.current_stats == null:
+		return {"attack": "--", "hit": "--", "crit": "--"}
 
-	# Check equipped weapon presence
-	var weapon: WeaponItemData = attacker.equipped_weapon
-	if weapon == null:
-		return {"damage": "--", "hit": "--", "crit": "--"}
+	var weapon := ally.equipped_weapon as WeaponItemData
+	var str := ally.current_stats.strength
+	var dex := ally.current_stats.dexterity
+	var faith := ally.current_stats.faith
 
-	# Make sure stats exist before accessing them
-	if attacker.current_stats == null or defender.current_stats == null:
-		return {"damage": "--", "hit": "--", "crit": "--"}
+	var attack := 0
+	var hit := 0
+	var crit := 0
+	var effectiveness := 1
 
-	var atk_stat: int = attacker.current_stats.strength
-	var def_stat: int = defender.current_stats.defense
+	# ========== ATTACK ==========
+	if weapon != null:
+		# Check effectiveness only if weapon and enemy class exist
+		if weapon.effective_against != WeaponItemData.Effectiveness.NONE and enemy != null and enemy.current_class != null:
+			if weapon.Effectiveness == enemy.current_class.ClassType:
+				effectiveness = 2
+		attack = str + int(weapon.power * effectiveness)
+	else:
+		# No weapon equipped: just base STR
+		attack = str
 
-	# (rest of the code unchanged)
-	var effectiveness_multiplier: int = 1
-	if weapon.effective_against != WeaponItemData.Effectiveness.NONE:
-		if defender.current_class != null and defender.current_class.class_type == int(weapon.effective_against):
-			effectiveness_multiplier = 2
+	# ========== HIT ==========
+	if weapon != null:
+		var distance_penalty := weapon.distance_hit_penalty
+		hit = weapon.hit_chance + ((dex * 2) + int(faith / 2)) - distance_penalty
+	else:
+		hit = (dex * 2) + int(faith / 2)
 
-	var bonus_damage: int = 0
-	var total_power: int = atk_stat + weapon.power + bonus_damage
-	var raw_damage: int = total_power * effectiveness_multiplier - def_stat
-	var damage: int = max(0, raw_damage)
-
-	var distance_penalty: int = 0
-	var bonus_hit: int = 0
-	var hit: int = clamp(weapon.hit_chance + attacker.current_stats.dexterity + bonus_hit - distance_penalty, 0, 100)
-	var crit: int = clamp(weapon.crit_chance + int(attacker.current_stats.dexterity / 2), 0, 100)
+	# ========== CRIT ==========
+	if weapon != null:
+		crit = weapon.crit_chance + int((dex + faith) / 2)
+	else:
+		crit = int((dex + faith) / 2)
 
 	return {
-		"damage": damage,
-		"hit": hit,
-		"crit": crit
+		"attack": str(attack),
+		"hit": str(hit),
+		"crit": str(crit)
 	}
-static func get_combat_forecast(attacker: Unit, defender: Unit) -> CombatForecast:
-	# Defaults
-	var attacker_name = "--"
-	var defender_name = "--"
-	var attacker_weapon_name = "--"
-	var defender_weapon_name = "--"
-	var attacker_hp_current = "--"
-	var attacker_hp_max = "--"
-	var defender_hp_current = "--"
-	var defender_hp_max = "--"
-	var ally_damage = "--"
+
+
+static func get_combat_forecast(ally: Unit, enemy: Unit) -> CombatForecast:
+	var ally_name = "--"
+	var enemy_name = "--"
+	var ally_weapon_name = "--"
+	var enemy_weapon_name = "--"
+	var ally_hp_current = "--"
+	var ally_hp_max = "--"
+	var enemy_hp_current = "--"
+	var enemy_hp_max = "--"
+	var ally_attack = "--"
 	var ally_hit = "--"
 	var ally_crit = "--"
-	var enemy_damage = "--"
+	var enemy_attack = "--"
 	var enemy_hit = "--"
 	var enemy_crit = "--"
 
-	# Ally side
-	if attacker != null and attacker.unit_data != null:
-		attacker_name = attacker.unit_data.unit_name
-		attacker_weapon_name = attacker.equipped_weapon.name if attacker.equipped_weapon != null else "--"
-		attacker_hp_current = attacker.hp
-		attacker_hp_max = attacker.current_stats.max_hp if attacker.current_stats != null else "--"
-		
-		# Only calculate ally stats if weapon and stats exist
-		if attacker.equipped_weapon != null and attacker.current_stats != null and defender != null and defender.unit_data != null and defender.current_stats != null:
-			var ally_stats = calculate_stats(attacker, defender)
-			ally_damage = ally_stats.damage
-			ally_hit = ally_stats.hit
-			ally_crit = ally_stats.crit
+	if ally != null and ally.unit_data != null:
+		ally_name = ally.unit_data.unit_name
+		ally_weapon_name = ally.equipped_weapon.name if ally.equipped_weapon != null else "--"
+		ally_hp_current = ally.hp
+		ally_hp_max = ally.current_stats.max_hp if ally.current_stats != null else "--"
 
-	# Enemy side
-	if defender != null and defender.unit_data != null:
-		defender_name = defender.unit_data.unit_name
-		defender_weapon_name = defender.equipped_weapon.name if defender.equipped_weapon != null else "--"
-		defender_hp_current = defender.hp
-		defender_hp_max = defender.current_stats.max_hp if defender.current_stats != null else "--"
-		
-		# Only calculate enemy stats if weapon and stats exist
-		if defender.equipped_weapon != null and defender.current_stats != null and attacker != null and attacker.unit_data != null and attacker.current_stats != null:
-			var enemy_stats = calculate_stats(defender, attacker)
-			enemy_damage = enemy_stats.damage
-			enemy_hit = enemy_stats.hit
-			enemy_crit = enemy_stats.crit
+		if ally.current_stats != null:
+			var ally_stats = calculate_stats(ally, enemy)
+			ally_attack = ally_stats["attack"]
+			ally_hit = ally_stats["hit"]
+			ally_crit = ally_stats["crit"]
+
+	if enemy != null and enemy.unit_data != null:
+		enemy_name = enemy.unit_data.unit_name
+		enemy_weapon_name = enemy.equipped_weapon.name if enemy.equipped_weapon != null else "--"
+		enemy_hp_current = enemy.hp
+		enemy_hp_max = enemy.current_stats.max_hp if enemy.current_stats != null else "--"
+
+		if enemy.current_stats != null:
+			var enemy_stats = calculate_stats(enemy, ally)
+			enemy_attack = enemy_stats["attack"]
+			enemy_hit = enemy_stats["hit"]
+			enemy_crit = enemy_stats["crit"]
 
 	return CombatForecast.new(
-		ally_damage,
+		ally_attack,
 		ally_hit,
 		ally_crit,
-		attacker_name,
-		defender_name,
-		attacker_weapon_name,
-		defender_weapon_name,
-		attacker_hp_current,
-		attacker_hp_max,
-		defender_hp_current,
-		defender_hp_max,
-		enemy_damage,
+		ally_name,
+		enemy_name,
+		ally_weapon_name,
+		enemy_weapon_name,
+		ally_hp_current,
+		ally_hp_max,
+		enemy_hp_current,
+		enemy_hp_max,
+		enemy_attack,
 		enemy_hit,
 		enemy_crit
 	)
